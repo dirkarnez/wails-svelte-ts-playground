6205c1d72f9d569903450a81c04e7a28a5b19cc7
Dirk Arnez <smalldirkalex@gmail.com>
2025-09-08 19:49:43 +0800

    - update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -1,8 +1,18 @@
 @echo off
 
 set PREFIX=%USERPROFILE%\Downloads
+
+set GOBIN=%PREFIX%\go1.25.0.windows-amd64\go\bin
+set NODE_ROOT=%PREFIX%\node-v22.14.0-win-x64\node-v22.14.0-win-x64
+
 set PATH=^
+%GOBIN%;^
+%NODE_ROOT%;
 
 https://nodejs.org/dist/v22.14.0/node-v22.14.0-win-x64.zip
 https://go.dev/dl/go1.25.0.windows-amd64.zip
-go install github.com/wailsapp/wails/v2/cmd/wails@latest
+
+go install github.com/wailsapp/wails/v2/cmd/wails@latest &&^
+
+
+
