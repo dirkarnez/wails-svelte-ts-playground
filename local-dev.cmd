894d05d82b7ac2c157c3a20b6d338fee6aa946ab
dirkarnez <smalldirkalex@gmail.com>
2025-09-12 20:16:26 +0800

    - working

---
+++ local-dev.cmd
@@ -0,0 +1,23 @@
+@echo off
+
+set PREFIX=%USERPROFILE%\Downloads
+
+set GOPATH=%PREFIX%\gopath
+set GOROOT=%PREFIX%\go1.25.0.windows-amd64\go
+set GOBIN=%GOROOT%\bin
+set GOEXE=%GOBIN%\go.exe
+
+
+set NODEROOT=%PREFIX%\node-v22.14.0-win-x64\node-v22.14.0-win-x64
+set NODEEXE=%NODEROOT%\node.exe
+
+set PATH=^
+%windir%\System32;^
+%PREFIX%\PortableGit\bin;^
+%GOBIN%;^
+%NODEROOT%;
+
+cd myproject &&^
+wails dev &&^
+cd /d "%~dp0"
+echo done
